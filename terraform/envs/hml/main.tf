0d2cff2af46c81f84030416eb6068138aa68ae1c
Lucas Bahr <88952738+Lucasbahr@users.noreply.github.com>
2026-03-22 22:24:54 -0300

    Bugfix/fix terraform (#16)
    * feat: adicionado estrutura primaria da api
    
    * feat: configurado cloud do terraform
    
    * feat: corrigido ci
    
    * feat: corrigido ci
    
    * feat: corrigido ci dos env
    
    * feat: corrigido ci dos env
    
    * feat: adicionado instalador do terraform
    
    * feat: adicionado instalador do terraform
    
    * fix: corrigido terraform
    
    * fix: corrigido terraform
    
    * fix: corrigido terraform
    
    * fix: corrigido criacao do terraform
    
    * fix: ALTERADO ENVS
    
    * fix: ALTERADO ENVS
    
    * fix: ALTERADO ENVS
    
    * fix: correcao provider
    
    * fix: correcao provider
    
    * fix: correcao porta dockerfile
    
    * Homologation (#11)
    
    * Development (#2)
    
    * feat: adicionado estrutura primaria da api
    
    * feat: configurado cloud do terraform
    
    * feat: corrigido ci
    
    * feat: corrigido ci
    
    * feat: corrigido ci dos env
    
    * feat: corrigido ci dos env
    
    * feat: adicionado instalador do terraform
    
    * feat: adicionado instalador do terraform
    
    * fix: corrigido terraform (#3)
    
    * Bugfix/fix terraform (#4)
    
    * fix: corrigido terraform
    
    * fix: corrigido terraform
    
    * Bugfix/fix terraform (#5)
    
    * fix: corrigido terraform
    
    * fix: corrigido terraform
    
    * fix: corrigido terraform
    
    * Bugfix/fix terraform (#6)
    
    * fix: corrigido terraform
    
    * fix: corrigido terraform
    
    * fix: corrigido terraform
    
    * fix: corrigido criacao do terraform
    
    * fix: ALTERADO ENVS
    
    * fix: ALTERADO ENVS
    
    * Bugfix/fix terraform (#7)
    
    * fix: corrigido terraform
    
    * fix: corrigido terraform
    
    * fix: corrigido terraform
    
    * fix: corrigido criacao do terraform
    
    * fix: ALTERADO ENVS
    
    * fix: ALTERADO ENVS
    
    * fix: ALTERADO ENVS
    
    * Bugfix/fix terraform (#8)
    
    * fix: corrigido terraform
    
    * fix: corrigido terraform
    
    * fix: corrigido terraform
    
    * fix: corrigido criacao do terraform
    
    * fix: ALTERADO ENVS
    
    * fix: ALTERADO ENVS
    
    * fix: ALTERADO ENVS
    
    * fix: correcao provider
    
    * Bugfix/fix terraform (#9)
    
    * fix: corrigido terraform
    
    * fix: corrigido terraform
    
    * fix: corrigido terraform
    
    * fix: corrigido criacao do terraform
    
    * fix: ALTERADO ENVS
    
    * fix: ALTERADO ENVS
    
    * fix: ALTERADO ENVS
    
    * fix: correcao provider
    
    * fix: correcao provider
    
    * Bugfix/fix terraform (#10)
    
    * fix: corrigido terraform
    
    * fix: corrigido terraform
    
    * fix: corrigido terraform
    
    * fix: corrigido criacao do terraform
    
    * fix: ALTERADO ENVS
    
    * fix: ALTERADO ENVS
    
    * fix: ALTERADO ENVS
    
    * fix: correcao provider
    
    * fix: correcao provider
    
    * fix: correcao porta dockerfile
    
    * fix: correcao ambiente terraform
    
    * fix: adicionado permisao de tag
    
    * fix: corrigido ambiente env
    
    * fix: alterado pipe
    
    * fix: alterado fluxo dev

---
+++ terraform/envs/hml/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+
+  cloud {
+    organization = "lucasbahr12"
+    workspaces {
+      name = "meuct-api-hml"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  credentials = var.google_credentials
+}
+
+module "api" {
+  source = "../../modules/cloud_run"
+
+  service_name      = "meuct-api"
+  environment       = "hml"
+  region            = var.region
+  project_id        = var.project_id
+  image             = var.image
+
+  database_url      = var.database_url
+  secret_key        = var.secret_key
+  algorithm         = var.algorithm
+
+  ci_service_account = var.ci_service_account
+}
