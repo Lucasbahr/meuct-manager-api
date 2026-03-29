097b68aa9643c0d20fcf7e3720bcc2a602e2e3ea
lucas bahr <lucas_bahr@hotmail.com>
2026-03-29 19:40:04 -0300

    feat adicionado entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 echo "🚀 Starting container..."
 
 echo "👤 Creating admin if not exists..."
-python create_admin.py
+python ./app/create_admin.py
 
 echo "🔥 Starting API..."
 exec uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8080}
